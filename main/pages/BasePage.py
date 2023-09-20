2ba7b748a3c4b8034ca2409015293ddb6abbc380
dungha.tien <dungha.tien@outlook.com>
2023-09-20 16:20:58 +0700

    Update

--- main/pages/BasePage.py
+++ main/pages/BasePage.py
@@ -1,24 +1,46 @@
+import logging
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+logger = logging.getLogger(__name__)
+from selenium.webdriver.common.by import By
+
+
 class BasePage:
-        def __init__(self, driver):
-          self.driver = driver
+    def __init__(self, driver):
+        self.driver = driver
 
-        def do_click(self, byLocator):
-          WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(byLocator)).click()
+    def do_click(self, byLocator):
+        logger.info("Click in locator")
+        logger.info(byLocator)
+        WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located(byLocator)).click()
 
-        def do_sendKeys(self, byLocator, text):
-            WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(byLocator)).send_keys(text)
+    def do_sendKeys(self, byLocator, text):
+        WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located(byLocator)).send_keys(text)
 
-        def get_ElementText(self, byLocator):
-            element = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(byLocator))
-            return element.text
+    def get_ElementText(self, byLocator):
+        element = WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located(byLocator))
+        return element.text
 
-        def isEnabled(self, byLocator):
-            element = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(byLocator))
-            return bool(element)
+    def isEnabled(self, byLocator):
+        element = WebDriverWait(self.driver, 20).until(
+            EC.visibility_of_element_located(byLocator))
+        return bool(element)
 
-        def getTitle(self, title):
-            WebDriverWait(self.driver, 1).until(EC.title_is(title))
-            return self.driver.title  
+    def getTitle(self, title):
+        WebDriverWait(self.driver, 1).until(EC.title_is(title))
+        return self.driver.title
+    
+    def wait_for_element_visible(self, byLocator):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(byLocator))
+
+    def wait_for_element_clickable(self, byLocator):
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(byLocator))
+
+
+    def wait_for_loading_complete(self):
+        LOADING = (By.XPATH, "//span[text()='Loading...']")
+        WebDriverWait(self.driver, 20).until(EC.invisibility_of_element(LOADING))
--- test/step_defs/dc400_rpc_multiple_devices_step.py
+++ test/step_defs/dc400_rpc_multiple_devices_step.py
@@ -9,13 +9,15 @@ import sys
 from main.pages.LoginPage import LoginPage
 from main.pages.HomePage import HomePage
 from main.pages.DashBoardGroupPage import DashBroardGroup
+from main.pages.rpc_multiple_device_page import RPCMultipleDevicePage
 # Constants
 logger = logging.getLogger(__name__)
- 
+from time import sleep
+
 GeoSensorX = 'https://staging.geosensorx.ai'
- 
+
 # Scenarios
- 
+
 scenarios('../features/dc400_rpc_multiple_devices.feature')
 
 
@@ -26,34 +28,81 @@ scenarios('../features/dc400_rpc_multiple_devices.feature')
 def browser():
     logger.info("Create driver")
     driver = webdriver.Chrome()
-    #driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    driver.get(GeoSensorX)
+    # driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver.implicitly_wait = 30
     driver.maximize_window()
+    driver.get(GeoSensorX)
     yield driver
     # driver.quit()
 # Given Steps
+
 
 @given('Login GSX Cloud')
 def login_page(browser):
     logger.info("Login")
     browser.get(GeoSensorX)
     loginPage = LoginPage(browser)
-    loginPage.doLoginPage("phat.ngo+tenant-admin@logigear.com", "Y9!ynp7GY-XHEKWN")
+    loginPage.doLoginPage(
+        "phat.ngo+tenant-admin@logigear.com", "Y9!ynp7GY-XHEKWN")
 
-@when('Go to Dashboard groups - DC400 - FMS Dashboard')
-def select_DC400(browser):
-    homePage = HomePage(browser)
-    homePage.select_DC400()
-    
-@when('Select any device in Landing page to go to Driving Data page')
-def select_Device_LandingPage(browser):
-    dashBoardGroup = DashBroardGroup(browser)
-    dashBoardGroup.selectDeviceLandingPage()
+@given("Go to Dashboard groups > DC400 > RPC Multiple Devices DC400")
+def go_to_rpc_multiple_device(browser):
+    multiplePage = RPCMultipleDevicePage(browser)
+    sleep(10)
+    logger.info("Open daskboard link")
+    multiplePage.clickDashBoardGroupLink()
 
-@then('Select any device in Devices list of Driving Data page')
-def select_Device_DevicesList(browser):
-    dashBoardGroup = DashBroardGroup(browser)
-    dashBoardGroup.selectDeviceInList()
+    sleep(10)
+    logger.info("Click dc400 link")
+    multiplePage.clickOnLink()
+
+    sleep(15)
+    logger.info("Open dc400 dasboard button")
+    multiplePage.openDashboardDC400()
+
+@given(parsers.parse("Select an active Device {string} on the DC400 devices list"))
+def select_device(browser, deviceName):
+  multiplePage = RPCMultipleDevicePage(browser)
+  logger.info("Click search device button")
+  multiplePage.wait_for_loading_complete()
+  multiplePage.clickButtonSearchDevice()
+
+  logger.info('Enter device name')
+  multiplePage.inputDeviceName(deviceName)
+
+  logger.info('Select device')
+  multiplePage.selectDevice(deviceName)
 
 
-    
+@given("Select {string} on RPC Method Description list")
+def select_rpc_method(browser, method):
+  multiplePage = RPCMultipleDevicePage(browser)
+  logger.info('Select RPC Method Description')
+  multiplePage.selectRPCMethod(method)
+
+
+@given('Enter {string} on RPC parameters')
+def select_rpc_method(browser, params):
+  multiplePage = RPCMultipleDevicePage(browser)
+  logger.info('Enter on RPC parameters')
+  multiplePage.inputRPCParameter(params)
+
+@given("Uncheck Clear selection after RPC ?")
+def uncheck_clear_selecttion_rpc(browser):
+   multiplePage = RPCMultipleDevicePage(browser)
+   logger.info("Uncheck Clear selection after RPC ?")
+   multiplePage.uncheck_clear_selection_after_rpc()
+
+
+@given("Click Send RPC button")
+def clickSendRPCButton(browser):
+   multiplePage = RPCMultipleDevicePage(browser)
+   logger.info("Click Send RPC button")
+   multiplePage.clickSendRPCButton()
+
+
+@given("Clear response history")
+def clear_response_history():
+    multiplePage = RPCMultipleDevicePage(browser)
+    logger.info("Clear response history")
+    multiplePage.clickClearRPCHistoryResponseButton();
